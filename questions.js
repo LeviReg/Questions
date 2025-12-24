848206f99cedfca5f50f89d515906cb79cafc5f6
Levi Regan <leviarcanine22@gmail.com>
2025-12-24 12:39:14 +0000

    Adding some more complex questions.

--- questions.js
+++ questions.js
@@ -13,3 +13,15 @@
 //car object
 
 //modify array of data
+
+//write a function that converts Farenheit to Celcius
+
+//write a function to find the larges number in an array of numbers
+
+//Fizz buzz function, a loop that counts from 1 to 50 If a number is divisible by 3, log "Fizz". If divisible by 5, log "Buzz". If divisible by both 3 and 5, log "FizzBuzz". Otherwise, just log the number.
+
+//Array Filtering, Create an array of ages: [12, 18, 25, 10, 33, 16]. Use the .filter() method to create a new array that only contains ages 18 and older.
+
+//Write a function that reverses a string (e.g. "Javascript" to "tpircsavaJ")
+
+//Write a funtion to count the vowels of a string passed
